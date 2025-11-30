9b76ddceb4b12421c0681dc3d818efab0ec16959
Muhammad <muminmuhsina1@gmail.com>
2025-11-30 15:05:03 -0800

    feat: create filter slice with actions for category, price, sort, and pagination

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productReducer from './slices/productSlice'
+import filterReducer from './slices/filterSlice'
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    filters: filterReducer,
   },
 })
 
